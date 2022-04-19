9d5b46a76918a88d8ee0edb298969db9c3d632bf
dfaroughy <63310570+dfaroughy@users.noreply.github.com>
2022-04-19 16:29:55 +0200

    Update bibminer.py

--- bibminer.py
+++ bibminer.py
@@ -1,21 +1,19 @@
-import numpy as np
 import urllib3
 import sys
 import json
-from collections import OrderedDict
-http = urllib3.PoolManager()
-
+import random
 class get_bibtex(object):
 
     def __init__(self,cite):
- 
+        
+        self.ids={'bibtexid': None, 'eprint': None, 'inspireid':None} 
         self.bibtexid=False    #    bibtex identifier e.g. Witten:123abc  
         self.eprint=False      #    arxiv numbers e.g. 2022.12345 , hep-ph/123456 
         self.inspireid=False   #    hep inspire id e.g 1234567
         self.unknown=False      
         self.cite=str(cite) 
-        self.ids={'bibtexid': None, 'eprint': None, 'inspireid':None}
-        
+        http = urllib3.PoolManager()
+
         try:
             if ':' in self.cite: 
                 url='https://inspirehep.net/api/literature?q={}'.format(cite)
@@ -25,13 +23,16 @@ class get_bibtex(object):
                 resp = http.request('GET', url)
                 out=resp.data.decode('utf-8') 
                 self.bibtex=out
+
                 #==============
                 if '"status": 404' not in out:
                     out=out.replace(' ',''); out=out.replace('\n','');
                     out=out.replace('"',''); out=out.split(',')
+
                     for x in out:
                         if 'eprint'in x: self.ids['eprint']=x.split('=')[-1]
                         if '@' in x: self.ids['bibtexid']=x.split('{')[-1]
+
                     self.bibtexid=True
                 
             elif self.cite.isdigit(): 
@@ -39,28 +40,34 @@ class get_bibtex(object):
                 resp = http.request('GET', url)
                 out=resp.data.decode('utf-8')
                 self.bibtex=out
+
                 #==============
                 if '"status": 404' not in out:
                     out=out.replace(' ',''); out=out.replace('\n','');
                     out=out.replace('"',''); out=out.split(',')
+
                     for x in out:
                         if 'eprint'in x: self.ids['eprint']=x.split('=')[-1]
                         if '@' in x: self.ids['bibtexid']=x.split('{')[-1]
+
                     self.ids['inspireid']=self.cite
                     self.inspireid=True
                 
-            elif ('.' in self.cite or 'hep-' in self.cite):        
+            elif ('.' in self.cite or 'hep-' in self.cite):  
                 url = 'https://inspirehep.net/api/arxiv/{}?format=bibtex'.format(cite)
                 resp = http.request('GET', url)
                 out=resp.data.decode('utf-8')
                 self.bibtex=out
+
                 #==============
                 if '"status": 404' not in out:
                     out=out.replace(' ',''); out=out.replace('\n','');
                     out=out.replace('"',''); out=out.split(',')
+
                     for x in out:
                         if 'eprint'in x: self.ids['eprint']=x.split('=')[-1]
-                        if '@' in x: self.ids['bibtexid']=x.split('{')[-1]                    
+                        if '@' in x: self.ids['bibtexid']=x.split('{')[-1]                  
+
                     url='https://inspirehep.net/api/literature?q={}'.format(self.ids['bibtexid'])
                     resp = http.request('GET', url)
                     self.ids['inspireid']=int(json.loads(resp.data.decode('utf-8'))['hits']['hits'][0]['id'])
@@ -72,26 +79,30 @@ class get_bibtex(object):
         
         except IndexError or KeyError:
             self.unknown=True  
-            self.bibtex={"status": 404, "message": "PID does not exist."}
+            self.bibtex={"status": 404, "message": "PID does not exist"}
             self.ids={'bibtexid': None, 'eprint': None, 'inspireid':None}
 
 
 def make_bib(tex, output=False, verbose=True):
-    tex_cites={}; linedic={}; bibdic={}
-    if not output:
-        output=tex.split('.')[0]+'.bib'    
+    
+    tex_cites={}
+    if not output: output=tex.split('.')[0]+'.bib'    
     bib=open(output,'w')
+    ran=hex(random.getrandbits(100))
+
     with open(tex,encoding="ascii", errors="surrogateescape") as latex:
         n=0
         for line in latex:
             n+=1
             line=line.replace(' ','')
-            line=line.replace('\cite{',' __________')
+            line=line.replace('\cite{',' {}'.format(ran))
             line=line.replace('}',' ')
+
             for l in line.split():         
-                if '__________' in l:
-                    l=l.replace('__________',' ')
+                if str(ran) in l:
+                    l=l.replace(str(ran),' ')
                     l=l.replace(',',' ')
+
                     for cite in l.split():
                         if cite not in tex_cites.keys(): tex_cites[cite]=[str(n)]
                         else: tex_cites[cite].append(str(n))
@@ -101,14 +112,13 @@ def make_bib(tex, output=False, verbose=True):
         if cite.unknown:
             print(u'l.{}| {} -----------<ERROR {}! {}> {}'.format(','.join(tex_cites[b]),b,cite.bibtex["status"],cite.bibtex["message"],'\u2717'))
         elif b!=cite.ids['bibtexid']:
-            bibdic[b]=cite.ids['bibtexid']
             if verbose: print(u'l.{}| {} -----------<{}> {}'.format(','.join(tex_cites[b]), b,cite.ids['bibtexid'],'\u2714'))
             cite.bibtex=cite.bibtex.replace(cite.ids['bibtexid'],b)
             bib.write(cite.bibtex)
         else:
-            bibdic[b]=cite.ids['bibtexid']
             if verbose: print(u'l.{}| {} {}'.format(','.join(tex_cites[b]),b,'\u2714'))
             bib.write(cite.bibtex)
+
     print('done!')
     bib.close()
 
