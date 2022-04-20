c0f0789ea5b0b24a0d90a8623c5a3524c38ce693
dfaroughy <63310570+dfaroughy@users.noreply.github.com>
2022-04-20 21:53:12 +0200

    renaming stuff again

--- bibminer.py
+++ bibminer.py
@@ -3,45 +3,48 @@ import sys
 import json
 import random
 
-class bibtex_entry(object):
+class bibtex(object):
 
-    def __init__(self,cite):
+    def __init__(self,bib_key):
         
-        self.cite=str(cite) 
+        self.bib=str(bib_key) 
+        self.entry=None
         self.inspire={'eprint':None, 'key':None,  'id':None }
         self.is_eprint=False         # arxiv numbers e.g. 2022.12345, hep-ph/123456
-        self.is_id=False     # hep inspire id e.g 1234567
-        self.is_missing=False 
+        self.is_id=False             # hep inspire id e.g 1234567
         self.is_custom=False
+        self.is_missing=False 
+
+
         try: 
-            float(self.cite)
+            float(self.bib)
             self.is_eprint=True                           
         except ValueError:
-            if 'hep-' in self.cite: self.is_eprint=True
-        if self.cite.isdigit(): self.is_id=True    
-        if '<' in self.cite and '>' in self.cite: self.is_custom=True
+            if 'hep-' in self.bib: self.is_eprint=True
+        if self.bib.isdigit(): self.is_id=True    
+        if '<' in self.bib and '>' in self.bib: self.is_custom=True
 
         http = urllib3.PoolManager()
 
         try:                
             if self.is_id: 
-                url = 'https://inspirehep.net/api/literature/{}?format=bibtex'.format(self.cite)
+                url = 'https://inspirehep.net/api/literature/{}?format=bibtex'.format(self.bib)
                 resp = http.request('GET', url)
                 out=resp.data.decode('utf-8')
-                self.bib=out
+                self.entry=out
                 #==============
                 out=out.replace(' ',''); out=out.replace('\n','');
                 out=out.replace('"',''); out=out.split(',')
                 for x in out:
                     if 'eprint'in x: self.inspire['eprint']=x.split('=')[-1]
                     if '@' in x: self.inspire['key']=x.split('{')[-1]
-                self.inspire['id']=self.cite
+                self.inspire['id']=self.bib
                 
             elif self.is_eprint:        
-                url = 'https://inspirehep.net/api/arxiv/{}?format=bibtex'.format(self.cite)
+                url = 'https://inspirehep.net/api/arxiv/{}?format=bibtex'.format(self.bib)
                 resp = http.request('GET', url)
                 out=resp.data.decode('utf-8')
-                self.bib=out
+                self.entry=out
                 #==============               
                 out=out.replace(' ',''); out=out.replace('\n','');
                 out=out.replace('"',''); out=out.split(',')
@@ -53,23 +56,23 @@ class bibtex_entry(object):
                 self.inspire['id']=int(json.loads(resp.data.decode('utf-8'))['hits']['hits'][0]['id'])
             
             elif self.is_custom:
-                custom=self.cite.replace('<','')
+                custom=self.bib.replace('<','')
                 custom=custom.replace('>',' ')
                 custom=custom.split()
                 title=custom[0].replace('_',' '); authors=custom[1:-1]; year=custom[-1]
                 authors=[a.replace(';',', ') for a in authors]
                 authors=' and '.join(authors)
                 self.inspire = self.inspire.fromkeys(self.inspire,str(None))
-                self.bib='@article{'+self.cite+',\n    author = "'+authors+'",\n    title = "'+title+'",\n    year = "'+year+'"}\n'
+                self.entry='@article{'+self.bib+',\n    author = "'+authors+'",\n    title = "'+title+'",\n    year = "'+year+'"}\n'
 
             else:
-                url='https://inspirehep.net/api/literature?q={}'.format(self.cite)
+                url='https://inspirehep.net/api/literature?q={}'.format(self.bib)
                 resp = http.request('GET', url)
                 self.inspire['id']=int(json.loads(resp.data.decode('utf-8'))['hits']['hits'][0]['id'])
                 url = 'https://inspirehep.net/api/literature/{}?format=bibtex'.format(self.inspire['id'])
                 resp = http.request('GET', url)
                 out=resp.data.decode('utf-8') 
-                self.bib=out
+                self.entry=out
                 #==============
                 out=out.replace(' ',''); out=out.replace('\n','');
                 out=out.replace('"',''); out=out.split(',')
@@ -79,13 +82,13 @@ class bibtex_entry(object):
         
         except (IndexError or KeyError):
             self.is_missing=True  
-            self.bib={"status": 404, "message": "PID does not exist."}
+            self.entry={"status": 404, "message": "PID does not exist."}
             self.inspire={'key': None, 'eprint': None, 'id':None}
 
 
 def make_bibtex_file(tex, output=False, verbose=True):
     
-    tex_cites={}
+    citations={}
     if not output: output=tex.split('.')[0]+'.bib'    
     rand=hex(random.getrandbits(100))
 
@@ -104,26 +107,26 @@ def make_bibtex_file(tex, output=False, verbose=True):
                     l=l.replace(',',' ')
                     
                     for cite in l.split():
-                        if cite not in tex_cites.keys(): tex_cites[cite]=[str(n)]
-                        else: tex_cites[cite].append(str(n))
+                        if cite not in citations.keys(): citations[cite]=[str(n)]
+                        else: citations[cite].append(str(n))
 
-    bib=open(output,'w',encoding="ascii")
-    bib.write('% Automatically generated with BiBminer 3000 (https://github.com/dfaroughy/BiBMiner)\n\n')
+    bibtex_file=open(output,'w',encoding="ascii")
+    bibtex_file.write('% Automatically generated with BiBminer 3000 (https://github.com/dfaroughy/BiBMiner)\n\n')
     
-    for Ref in tex_cites.keys():
+    for ref in citations.keys():
         #=================
-        cite=bibtex_entry(Ref)
+        bib=bibtex(ref)
         #=================
-        if cite.is_missing:
-            print(u'l:{}| {} ---------<ERROR {}! {}> {}'.format(','.join(tex_cites[Ref]),Ref,cite.bib["status"],cite.bib["message"],'\u2717'))
+        if bib.is_missing:
+            print(u'l:{}| {} ---------<ERROR {}! {}> {}'.format(','.join(citations[ref]),ref,bib.entry["status"],bib.entry["message"],'\u2717'))
 
         else:
-            if verbose: print(u'l:{}| {} {}'.format(','.join(tex_cites[Ref]),Ref,'\u2714'))
-            if cite.inspire['key']!=Ref: cite.bib=cite.bib.replace(cite.inspire['key'],Ref)
-            cite.bib = cite.bib.encode('ascii', 'ignore').decode('ascii')
-            bib.write(cite.bib)
+            if verbose: print(u'l:{}| {} {}'.format(','.join(citations[ref]),ref,'\u2714'))
+            if bib.inspire['key']!=ref: bib.entry=bib.entry.replace(bib.inspire['key'],ref)
+            bib.entry = bib.entry.encode('ascii', 'ignore').decode('ascii')
+            bibtex_file.write(bib.entry)
 
     print('done!')
-    bib.close()
+    bibtex_file.close()
 
 if len(sys.argv)==2: make_bibtex_file(tex=sys.argv[1])
